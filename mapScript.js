e5b65a22ed9b444b35d53b17b551c0a6cc712500
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2023-04-18 19:27:16 -0500

    test again

--- mapScript.js
+++ mapScript.js
@@ -1,33 +1,25 @@
-// Slider code from https://bxslider.com/
-$(document).ready(function(){
-	$('.slider').bxSlider();
-		
-});
-
 var map;
 const iit = {lat:41.835, lng:-87.627};
-const polska = {lat:52, lng:21};
-const goodwill = {lat:41.88284462952575, lng:-87.65705415769303};
+const severn = {lat:39.028843, lng:    -76.529090};
+const ocean = {lat:38.329228, lng:-75.085384};
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 15, center:iit,
-	});
-	const marker = new google.maps.Marker({
-		position: iit, map:map,
-	});
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15, center:iit,
+    });
+    const marker = new google.maps.Marker({
+        position: iit, map:map,
+    });
 }
-
 function ultraZoom() {
-	map.setZoom(30);
+    map.setZoom(30);
 }
 
-function goToPoland() {
-	map.setCenter(polska);
-	map.setZoom(6);
+function goToSevern() {
+    map.setCenter(severn);
+    map.setZoom(15);
 }
 
-function goToGoodwill() {
-	map.setCenter(goodwill);
-	map.setZoom(20);
-}
+function goToOcean() {
+    map.setCenter(ocean);
+    map.setZoom(15);}
