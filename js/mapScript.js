bc70e11638ed6883cfdaafb3b5afd46e2c9724d5
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2023-04-18 19:00:34 -0500

    agahgghasghasghgshagsaghsdgh

--- js/mapScript.js
+++ js/mapScript.js
@@ -1,27 +1,14 @@
-// Initialize and add the map
-let map;
+var map;
+const iit = {lat:41.835, lng:-87.627};
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("canvas"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15, center:iit,
+    });
+    const marker = new google.maps.Marker({
+        position: iit, map:map,
+    });
 }
 
 initMap();
