530a52419efff98a3e831be65423a5cc32e23b8c
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2023-04-18 19:08:08 -0500

    map no work me sad map make me sad please work

--- js/mapScript.js
+++ js/mapScript.js
@@ -1,14 +1,12 @@
 var map;
 const iit = {lat:41.835, lng:-87.627};
 
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15, center:iit,
-    });
-    const marker = new google.maps.Marker({
-        position: iit, map:map,
-    });
+	map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 15, center:iit,
+	});
+	const marker = new google.maps.Marker({
+		position: iit, map:map,
+	});
 }
 
-initMap();
