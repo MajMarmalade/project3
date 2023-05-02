c12a2c43d50a1508bac77bd1c913cbed41d7c0c1
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2023-05-02 16:34:16 -0500

    Lets try adding a marker you can click to prove the existence of aliens

--- js/script.js
+++ js/script.js
@@ -2,8 +2,26 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:37.629562, lng:-116.849556},
-    zoom: 10
+    zoom: 15
 });
 }
+
+const contentString = "<p>No man could ever make a large geometric object on the ground! This must be ALIENS</p>";
+
+const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Evidence 1",
+});
+ let marker = new google.maps.Marker({
+    position: {lat:37.629562, lng:-116.849556},
+    map,
+    title: "Evidence of Aliens 1"
+})
+marker.addListener("click", () => {
+    infowindow.open({
+    anchor: marker,
+    map,
+    });
+});
 
 initMap();
